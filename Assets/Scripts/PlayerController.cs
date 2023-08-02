41f502b56fb18cef8ea8a44827f6e4ce0371cac5
ClaireMilk <79968107+ClaireMilk@users.noreply.github.com>
2023-08-03 00:26:04 +0800

    movement
    移动有了 但是还有些许问题 改天再改

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PlayerController : MonoBehaviour
+{
+    public SteamVR_Action_Vector2 input;
+    public float speed;
+
+    void Update()
+    {
+        transform.position += speed * Time.deltaTime * new Vector3(input.axis.x, 0, input.axis.y);
+    }
+}
