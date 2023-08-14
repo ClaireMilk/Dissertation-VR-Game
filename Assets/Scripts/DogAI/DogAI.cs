bd5fdf1bf646301cefce8997a1b58e5939cf562c
MEITIANRIGOU <71801399+MEITIANRIGOU@users.noreply.github.com>
2023-08-14 19:25:02 +0800

    狗狗AI +动画

--- Assets/Scripts/DogAI/DogAI.cs
+++ Assets/Scripts/DogAI/DogAI.cs
@@ -6,21 +6,59 @@ public class DogAI : MonoBehaviour
 {
     public Transform targetTransform;
     Vector3 moveToPosition;
+    Vector3 moveToDirection;
+    public float moveSpeed;
+
+    Vector3 directionResult;
+    float angleResult;
+
+    public GameObject player;
+
+    Rigidbody dogRigidBody;
+    Animator dogAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        dogRigidBody = GetComponent<Rigidbody>();
+        dogAnimator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         moveToPosition = new Vector3(targetTransform.position.x, gameObject.transform.position.y, targetTransform.position.z);
-        //Debug.Log("Dog: " + gameObject.transform.position);
-        //Debug.Log("Target: " + moveToPosition);
-        if (gameObject.transform.position != moveToPosition)
+        moveToDirection = moveToPosition - gameObject.transform.position;
+
+        if ((gameObject.transform.position - moveToPosition).magnitude >= 0.05)
         {
-            gameObject.transform.position = moveToPosition;
+            dogRigidBody.velocity = moveToDirection.normalized * (moveSpeed + (gameObject.transform.position - moveToPosition).magnitude);
+            dogAnimator.SetBool("Moving", true);
+
+            directionResult = Vector3.Cross(gameObject.transform.forward, moveToDirection);
+            angleResult = Vector3.Angle(moveToDirection, gameObject.transform.forward);
+        }
+        else
+        {
+            dogRigidBody.velocity = Vector3.zero;
+            dogAnimator.SetBool("Moving", false);
+
+            Vector3 playerForward = player.transform.forward;
+            directionResult = Vector3.Cross(gameObject.transform.forward, playerForward);
+            angleResult = Vector3.Angle(playerForward, gameObject.transform.forward);
+        }
+
+
+        if (angleResult >= 5)
+        {
+            if (directionResult.y > 0)
+            {
+                gameObject.transform.Rotate(0, 200 * Time.deltaTime, 0);
+            }
+            else if (directionResult.y < 0)
+            {
+                gameObject.transform.Rotate(0, -200 * Time.deltaTime, 0);
+            }
         }
     }
 }
